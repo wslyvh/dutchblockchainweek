64a854273695c707a347e4a3386bd6ee088b45c7
wslyvh <wslyvh@users.noreply.github.com>
2019-04-29 20:49:00 +0200

    dutch flag

--- .vuepress/theme/Navbar.vue
+++ .vuepress/theme/Navbar.vue
@@ -13,61 +13,72 @@
 </template>
 
 <script>
-import SidebarButton from './SidebarButton.vue'
-import AlgoliaSearchBox from '@AlgoliaSearchBox'
-import SearchBox from './SearchBox.vue'
-import NavLinks from './NavLinks.vue'
+import SidebarButton from "./SidebarButton.vue";
+import AlgoliaSearchBox from "@AlgoliaSearchBox";
+import SearchBox from "./SearchBox.vue";
+import NavLinks from "./NavLinks.vue";
 
 export default {
   components: { SidebarButton, NavLinks, SearchBox, AlgoliaSearchBox },
   computed: {
-    algolia () {
-      return this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+    algolia() {
+      return (
+        this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
+      );
     },
-    isAlgoliaSearch () {
-      return this.algolia && this.algolia.apiKey && this.algolia.indexName
+    isAlgoliaSearch() {
+      return this.algolia && this.algolia.apiKey && this.algolia.indexName;
     },
-    logo () {
-      return require('./../public/logo.png')
+    logo() {
+      return require("./../public/logo.png");
     }
   }
-}
+};
 </script>
 
 <style lang="stylus">
-@import './styles/config.styl'
+@import './styles/config.styl';
 
-.navbar
-  padding 0.9rem 3.5rem
-  line-height $navbarHeight - 1.4rem
-  position relative
-  a, span, img
-    display inline-block
-  .logo
-    height $navbarHeight - 1.8rem
-    min-width $navbarHeight - 1.4rem
-    margin-right 0
-    vertical-align top
-  .site-name
-    font-size 1.3rem
-    font-weight 600
-    color #fff
-    position relative
-  .links
-    font-size 0.9rem
-    position absolute
-    right 3.5rem
-    top 0.7rem
+.navbar {
+  padding: 0.9rem 3.5rem;
+  line-height: $navbarHeight - 1.4rem;
+  position: relative;
 
-@media (max-width: $MQMobile)
-  .navbar
-    padding 0.9rem 1.5rem
-    .links
-      right 3.5rem
-    .logo
-      height 2rem
-      min-width 2rem
-    
-    .can-hide
-      display none
+  a, span, img {
+    display: inline-block;
+  }
+
+  .logo {
+    margin-right: 0;
+    vertical-align: top;
+  }
+
+  .site-name {
+    font-size: 1.3rem;
+    font-weight: 600;
+    color: #fff;
+    position: relative;
+  }
+
+  .links {
+    font-size: 0.9rem;
+    position: absolute;
+    right: 3.5rem;
+    top: 0.7rem;
+  }
+}
+
+@media (max-width: $MQMobile) {
+  .navbar {
+    padding: 0.9rem 1.5rem;
+
+    .links {
+      right: 3.5rem;
+    }
+
+    .can-hide {
+      display: none;
+    }
+  }
+}
 </style>
